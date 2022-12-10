9f39e9987f88dd77143dd80e14c8e52e544adbfa
rusanovschi1994 <rusanovschi1994@gmail.com>
2022-12-11 01:58:38 +0200

    tests with selenide

---
+++ src/main/java/pages/RegistrationPage.java
@@ -0,0 +1,88 @@
+package pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static java.lang.String.*;
+
+public class RegistrationPage {
+
+    private final By companyNameField = By.xpath("//input[@name=\"companyname\"]");
+    private final By emailField = By.xpath("//input[@name=\"email\"]");
+    private final By passwordField = By.cssSelector("input#password");
+    private final By confirmPasswordField = By.xpath("//input[@name=\"passwordConfirm\"]");
+    private final By promoCodeField = By.cssSelector("input#promocode");
+    private final By registrationButton = By.xpath("//button[@type=\"submit\"]");
+    private String languageOption =
+            "//a[@class=\"dropdown-toggle\"]/following-sibling::ul//a[normalize-space()=\"%s\"]";
+    private String errorByText = "//em[text()=\"%s\"]";
+    private By languageDropDown = By.xpath("//a[@class=\"dropdown-toggle\"]/parent::li");
+    private By errors = By.xpath("//em[@for]");
+
+    public RegistrationPage open(){
+
+        Selenide.open("/");
+        return this;
+    }
+
+    public RegistrationPage typeCompanyName(String name){
+
+        $(companyNameField).setValue(name);
+        return this;
+    }
+
+    public RegistrationPage typeEmailAddress(String email){
+
+        $(emailField).setValue(email);
+        return this;
+    }
+
+    public RegistrationPage typePassword(String password){
+
+        $(passwordField).setValue(password);
+        return this;
+    }
+
+    public RegistrationPage typeConfirmPassword(String password){
+
+        $(confirmPasswordField).setValue(password);
+        return this;
+    }
+
+    public RegistrationPage typePromoCode(String code){
+
+        $(promoCodeField).setValue(code);
+        return this;
+    }
+
+    public RegistrationPage selectLanguage(String option){
+
+        $(languageDropDown).click();
+        $(By.xpath(format(languageOption, option))).click();
+        return this;
+    }
+
+    public void clickRegistration(){
+
+        $(registrationButton).click();
+    }
+
+    public ElementsCollection getErrors(){
+
+        return $$(errors);
+    }
+
+    public SelenideElement getErrorByNumber(int number){
+
+        return $$(errors).get(number - 1);
+    }
+
+    public SelenideElement getError(String text){
+
+        return $(By.xpath(format(errorByText, text)));
+    }
+}
---
+++ src/test/java/RegistrationPageTest.java
@@ -0,0 +1,78 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Configuration.*;
+
+public class RegistrationPageTest {
+
+    private RegistrationPage page;
+
+    @BeforeClass
+    public static void setUp(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\selenium\\chromedriver.exe");
+        baseUrl = "https://provectapos.com/app/#/register";
+        browser = "chrome";
+    }
+
+    @Test
+    public void registerWithEmptyCompanyField(){
+
+        page = new RegistrationPage();
+        page
+                .open()
+                .selectLanguage("română")
+                .typeEmailAddress("test@test.test")
+                .typePassword("test")
+                .typeConfirmPassword("test")
+                .clickRegistration();
+        page.getError("Please enter your business name").shouldBe(visible);
+        page.getErrors().shouldHave(size(1));
+    }
+
+    @Test
+    public void typeInvalidEmail(){
+
+        page = new RegistrationPage();
+        page
+                .open()
+                .selectLanguage("English")
+                .typeCompanyName("Test")
+                .typeEmailAddress("test")
+                .typePassword("test")
+                .typeConfirmPassword("test")
+                .typePromoCode("test")
+                .clickRegistration();
+        page.getError("Please enter a VALID email address").shouldBe(visible);
+        page.getError("Please enter the same password as above").shouldNotBe(visible);
+    }
+
+    @Test
+    public void typeInvalidConfirmPassword(){
+
+        page = new RegistrationPage();
+        page
+                .open()
+                .typeCompanyName("Test")
+                .typeEmailAddress("test@gmail.com")
+                .typePassword("1111")
+                .typeConfirmPassword("1123")
+                .clickRegistration();
+        page.getError("Please enter the same password as above").shouldBe(visible);
+    }
+
+    @Test
+    public void registerWithEmptyFields(){
+
+        page = new RegistrationPage();
+        page
+                .open()
+                .selectLanguage("English")
+                .clickRegistration();
+        page.getErrors().shouldHave(size(4));
+        page.getErrorByNumber(3).shouldHave(text("Please enter your password"));
+    }
+}
